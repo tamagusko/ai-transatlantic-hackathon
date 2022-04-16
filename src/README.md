20a7df1172cc01302a06c20acce03329e3a77dbe
Tiago Tamagusko <tamagusko@gmail.com>
2022-04-16 00:56:31 +0000

    split sendSMS.py into two files

--- src/README.md
+++ src/README.md
@@ -1,4 +1,5 @@
 1. ~~sendSMS.py~~ @tamagusko
+	- Twilio Number: +18646971168
 2. makeCall.py
 3. coordinates.py
 4. closestLocker.py
--- src/sendSMS.py
+++ src/sendSMS.py
@@ -1,81 +1,42 @@
 # (c) Tiago Tamagusko 2022
+# Source: https://www.twilio.com/docs/sms/quickstart/python
 """
-Sends an sms asking if the client is at home to receive the order. Expected response: yes (y) or no (n).
-If the answer is yes (y), the delivery continues, if it is no (n) it generates a closest Locker request.
+Sends an sms using Twilio API.
 
 Usage:
-    $ python path/to/sendSMS.py --id DELIVERY_ID --phone NUMBER --time WAITING_TIME_IN_SECONDS
+    $ sendSMS(delivery_id, client_phone)
 
 Example:
-    # send a sms to the phone number 1234567890 and wait for a response for 2 minutes
-    $ python path/to/sendSMS.py --id 20220415_1 --phone 1234567890 --time 120
+    # send a sms to the phone number 1234567890
+    $ sendSMS(20220415_1, 1234567890)
 """
 from __future__ import annotations
 
-import argparse
 import os
 
 from dotenv import load_dotenv
 from twilio.rest import Client
 
 load_dotenv()  # secrets are saved on .env file
+# .env: TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE
 
 
-def main():
-    # Parser
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description='Send sms and wait for response (y or n)',
-    )
-    parser.add_argument(
-        '-i', '--id',
-        type=str,
-        required=True,
-        help='Delivery id',
-    )
-    parser.add_argument(
-        '-p', '--phone',
-        type=str,
-        required=True,
-        help='Provide phone for shipping. Supported formats: 222333444, 222,333,444, 222-333-444, 222 333 444',
-    )
-    parser.add_argument(
-        '-t', '--time',
-        type=int,
-        default=300,  # 5 minutes
-        help='Time to wait for client response',
-    )
-    args = parser.parse_args()
+def sendSMS(delivery_id: str, phone: str):
+    # authenticate with twilio
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
 
-    def sendSMS(delivery_id, phone, waiting_time):
-        text_to_send = f'Delivery {delivery_id} should take approximately 1 hour.  \
-            Are you at home to receive the order? Reply y or n in the next {waiting_time} to confirm delivery.'
-        account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-        auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-        client = Client(account_sid, auth_token)
+    # gen the message
+    text_to_send = f'Delivery {delivery_id} should take approximately 1 hour.  \
+        Are you at home to receive the order? Reply y or n to confirm delivery.'
 
-        message = client.messages.create(
+    # send the message
+    message = client.messages \
+        .create(
             body=text_to_send,
-            from_=os.getenv('TWILIO_PHONE_NUMBER'),
+            from_=os.getenv('TWILIO_PHONE'),
             # SMS messages sent limited to phone numbers that have been verified with Twilio (trial version)
-            to=os.getenv('CLIENT1_PHONE_NUMBER'),
+            to=phone,
         )
-        print(message.sid)
-        sendSMS(args.id, args.phone, args.time)
-
-        def get_response(delivery_id, phone, waiting_time):
-            account_sid = os.getenv('TWILIO_ACCOUNT_SID')
-            auth_token = os.getenv('TWILIO_AUTH_TOKEN')
-            client = Client(account_sid, auth_token)
-
-            message = client.messages.list(
-                to=os.getenv('CLIENT1_PHONE_NUMBER'),
-                from_=os.getenv('TWILIO_PHONE_NUMBER'),
-                limit=1,
-            )
-            print(message.sid)
-            sendSMS(args.id, args.phone, args.time)
-
-
-if __name__ == '__main__':
-    main()
+    return message.sid
