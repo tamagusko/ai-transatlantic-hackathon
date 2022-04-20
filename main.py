b75294299395371e68895ab8b20501650b234011
Tiago Tamagusko <tamagusko@gmail.com>
2022-04-20 18:55:20 +0000

    fix: fastapi is working

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 from fastapi import FastAPI
 
 from src.coordinates import router as coordinates
-from src.test import router as test
 
 app = FastAPI(
     title='MVP',
@@ -20,7 +19,6 @@ def index():
 
 
 app.include_router(coordinates)
-app.include_router(test)
 # app.include_router(make_call)
 # app.include_router(send_email)
 # app.include_router(send_sms)
