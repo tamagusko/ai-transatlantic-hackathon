9897ba23b5ed40d62a1131ac170702d8a8aa843e
Tiago Tamagusko <tamagusko@gmail.com>
2022-04-23 00:48:53 +0000

    fix: split coordinates into latitude and longitude

--- src/processingData.py
+++ src/processingData.py
@@ -14,18 +14,22 @@ df = pd.read_csv(
 )
 
 
-def saveData(df, folder='./data/processed/', filename='clientAddressCoordinates.csv', index_col=0):
+def saveData(df, folder='./data/processed/', filename='clientCoordinates.csv', index_col=0):
     """Save a dataframe to a csv file"""
     return df.to_csv(folder + filename, sep=';', encoding='utf-8')
 
 
 def add_client_coordinates_to_dataset(df):
     """
-    Returns the coordinates of a client list.
+    Returns the coordinates (latitude and longitude) of a client list.
     Args:
-        dataset: The dataset must have two columns, one for the clients and one for the address.
+        dataset: The dataset must have two columns, one for the clients and another for the address.
     """
-    df['coordinates'] = df['address'].apply(coordinates)
+    df['coordinates'] = df['address'].apply(coordinates).astype(str)
+    df = df['coordinates'].str.strip('()').str.split(
+        ', ',
+    ).apply(pd.Series).astype(float)
+    df = df.rename(columns={0: 'latitude', 1: 'longitude'})
     return df
 
 
