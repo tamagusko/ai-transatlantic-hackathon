0e2b3da195f27eed2c59181d1e3320de3738b62b
Tiago Tamagusko <tamagusko@gmail.com>
2022-04-23 01:24:08 +0000

    refactor: simplifying

--- src/processingData.py
+++ src/processingData.py
@@ -5,12 +5,15 @@ Add coordinates to the dataset.
 from __future__ import annotations
 
 import pandas as pd
-from coordinates import coordinates
+from coordinates import latitude
+from coordinates import longitude
 
 
 df = pd.read_csv(
-    './data/clientAddress.csv', sep=';',
-    encoding='utf-8', index_col=0,
+    './data/clientAddress.csv',
+    sep=';',
+    encoding='utf-8',
+    index_col=0,
 )
 
 
@@ -25,11 +28,8 @@ def add_client_coordinates_to_dataset(df):
     Args:
         dataset: The dataset must have two columns, one for the clients and another for the address.
     """
-    df['coordinates'] = df['address'].apply(coordinates).astype(str)
-    df = df['coordinates'].str.strip('()').str.split(
-        ', ',
-    ).apply(pd.Series).astype(float)
-    df = df.rename(columns={0: 'latitude', 1: 'longitude'})
+    df['latitude'] = df['address'].apply(latitude).astype(str)
+    df['longitude'] = df['address'].apply(longitude).astype(str)
     return df
 
 
